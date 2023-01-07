7bcdb9e2a4361ba724078a4b17d3f6e6ab8d2e2c
YounnesYBN <younnesYBN@outlook.com>
2023-01-07 12:27:11 +0100

    adding module

--- view/components/AnalyseResultatsEFMAccompagnentStagaires.php
+++ view/components/AnalyseResultatsEFMAccompagnentStagaires.php
@@ -5,37 +5,3 @@ if ($_SESSION["pass"] == false) {
 
 ?>
 
-<tr>
-    <td rowspan="6" class="tds">Analyse resultats EFM et accompagnent des stagaires</td>
-</tr>
-
-<tr>
-    <td>Effectif des stagaires en formation</td>
-    <td><input type="number" name=""></td>
-    <td><input type="text" name=""></td>
-</tr>
-
-<tr>
-    <td>Nombre de stagaires en difficultés</td>
-    <td><input type="number" name=""></td>
-    <td><input type="text" name=""></td>
-</tr>
-
-<tr>
-    <td>Les raisons de la situation des stagaires en difficultés</td>
-    <td><input type="number" name=""></td>
-    <td><input type="text" name=""></td>
-</tr>
-
-<tr>
-    <td>Les action mises en place pour la régularisation de la situation</td>
-    <td><input type="number" name=""></td>
-    <td><input type="text" name=""></td>
-</tr>
-
-<tr>
-    
-    <td>Les propositions d'amélioration</td>
-    <td><input type="number" name=""></td>
-    <td><input type="text" name=""></td>
-</tr>
--- view/components/ValidationDesGroups.php
+++ view/components/ValidationDesGroups.php
@@ -8,36 +8,3 @@ if ($_SESSION["pass"] == false) {
 ?>
 
 
-<tr>
-   <td rowspan="6" class="tds">validation des groupes - Directeur</td> 
-</tr>
-<tr>
-    
-   <td>Nombre total des groupes</td>
-   <td><input type="number" name="" id=""></td>
-   <td><input type="text" name="" id=""></td>
-</tr>
-<tr>
-    
-    <td> Nombre total des groupes valides</td>
-    <td><input type="number" name="" id=""></td>
-    <td><input type="text" name="" id=""></td>
-</tr>
-<tr>
-    
-    <td>les raison des groupes non validés</td>
-    <td><input type="number" name="" id=""></td>
-    <td><input type="text" name="" id=""></td>
-</tr>
-<tr>
-    
-    <td> Les action mises en place pour la regularisation de la situation</td>
-    <td><input type="number" name="" id=""></td>
-    <td><input type="text" name="" id=""></td>
-</tr>
-<tr>
-    
-    <td> les propositions d'amelioration</td>
-    <td><input type="number" name="" id=""></td>
-    <td><input type="text" name="" id=""></td>
-</tr>
