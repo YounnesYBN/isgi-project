a23005d022949d35652a8ff8fdc49ef6d41d66fc
YounnesYBN <younnesYBN@outlook.com>
2023-01-07 12:52:55 +0100

    add all other modules

---
+++ app/module/aspeets_traiter.php
@@ -0,0 +1,26 @@
+<?php 
+class Aspeets_traiter{
+    private $id ;
+    private $libelle ;
+
+    public function __construct($id,$libelle)
+    {
+        $this->id = $id ;
+        $this->libelle = $libelle ;
+    }
+
+    public function GetId(){
+        return $this->id ;
+    }
+    public function GetLibelle(){
+        return $this->libelle ;
+    }
+    public function SetId($p){
+         $this->id  = $p;
+    }
+    public function SetLibelle($p){
+         $this->libelle  = $p;
+    }
+}
+
+?>
