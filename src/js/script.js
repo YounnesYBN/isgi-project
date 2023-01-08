c25e496d3fdcc3d69198fe2148d096c7920eab26
YounnesYBN <younnesYBN@outlook.com>
2023-01-08 16:03:27 +0100

    fixing a popup

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
 
-document.querySelector("#btn-select").addEventListener("click",()=>{
-    document.getElementById("popupCon").style.display = "flex";
-})
+var allAddOptButs = document.querySelectorAll("#btn-select")
+
+for (let index = 0; index < allAddOptButs.length; index++) {
+    const element = allAddOptButs[index];
+    element.addEventListener("click",()=>{
+        document.getElementById("popupCon").style.display = "flex";
+    })
+    
+}
 
 document.querySelector("#xbutton").addEventListener("click",()=>{
     document.getElementById("popupCon").style.display = "none";
