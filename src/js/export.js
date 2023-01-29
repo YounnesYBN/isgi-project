14cef9a4b6ab76c67b9b9e47585488a1ad57c710
Mustapha-Amzil <mustaphaamzil124@gmail.com>
2023-01-29 22:20:13 +0100

    add some changes

--- src/js/export.js
+++ src/js/export.js
@@ -1,13 +1,13 @@
 var table2excel = new Table2Excel();
-    document.getElementById("btn-valide").addEventListener("click",()=>{
-        let AllInputNumber = document.querySelectorAll("input[type='number']")
-        AllInputNumber.forEach( Element=> {
-            Element.type = "text"
-        });
-        table2excel.export(document.getElementById("tableGenerale"));
-        AllInputNumber.forEach( Element=> {
-            Element.type = "number"
-        });
+document.getElementById("btn-valide").addEventListener("click", () => {
+  let AllInputNumber = document.querySelectorAll("input[type='number']");
+  AllInputNumber.forEach((Element) => {
+    Element.type = "text";
+  });
+  table2excel.export(document.getElementById("tableGenerale"));
+  AllInputNumber.forEach((Element) => {
+    Element.type = "number";
+  });
 
-        location.reload() 
-    })
+  location.reload();
+});
